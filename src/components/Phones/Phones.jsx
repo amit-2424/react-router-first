14abb9ab64c2aeace96e430c7b1a9292cc0a1ff4
oishik <amitmondoloishik@gmail.com>
2024-03-20 21:12:37 +0600

    second commit

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar,  XAxis, YAxis, Tooltip } from 'recharts';
 
 
 const Phones = () => {
